62a168cd968d481325d00f234b8c0c9e8567213f
kod-python <kelvinlik0245@gmail.com>
2024-07-12 13:48:11 +0000

    another one

--- src/components/Slide/Slide.tsx
+++ src/components/Slide/Slide.tsx
@@ -1,5 +1,5 @@
 'use client'
-
+/* eslint-disable react/no-unescaped-entities */
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/swiper-bundle.css';
 import { Navigation } from 'swiper/modules';
@@ -61,7 +61,7 @@ const SwiperPage = () => {
           <div className='m-[40px]'>
 <h2 className='text-center font-bold text-lg'>Track Driver</h2>
           <p className='p-[50px] mt-[-40px] text-center'>
-            Stay updated in real-time on your keke &apos;driver's location to know exactly
+            Stay updated in real-time on your keke driver's location to know exactly
             when they will arrive!
           </p>
 </div>
