7cf59fccf15737281b62a4c54a25619ab50156bb
kod-python <kelvinlik0245@gmail.com>
2024-07-14 00:50:27 +0000

    space commit

--- src/components/Slide/Slide.tsx
+++ src/components/Slide/Slide.tsx
@@ -29,9 +29,9 @@ const SwiperPage = () => {
           <Image src={k3k32} alt='k3k31' className='w-full h-[100vh]  m-auto'></Image>
 <div className='mt-[40px]'>
 <h2 className='text-center font-bold text-[1.5rem]'>Request Ride</h2>
-          <p className='p-[20px] mt-[30px] text-center'>
-            Discover hassle-free keke bokkings with just a tap.
-            Wherever you are headed, weve got you covered
+          <p className='p-[20px] text-center'>
+            Discover hassle-free keke bookings with just a tap.
+            Wherever you are headed, we got you covered
           </p>
 </div>
           
@@ -44,8 +44,8 @@ const SwiperPage = () => {
           <Image src={k3k33} alt='k3k31' className='w-[100%] h-[100vh] m-auto'  ></Image>
           <div className='mt-[40px]'>
 <h2 className='text-center font-bold text-[1.5rem]'>Book Driver</h2>
-          <p className='p-[20px] mt-[10px] text-center'>
-            Easily book a reliable keke driver through our app.Enjoy a smooth
+          <p className='p-[20px]  text-center'>
+            Easily book a reliable keke driver through our app. Enjoy a smooth
             journey to your destination!
           </p>
 </div>
@@ -60,7 +60,7 @@ const SwiperPage = () => {
           <Image src={k3k34} alt='k3k31' className='w-[100%]  h-[95vh] m-auto'></Image>
           <div className='m-[40px]'>
 <h2 className='text-center font-bold text-[1.5rem]'>Track Driver</h2>
-          <p className='p-[20px] mt-[10px] text-center'>
+          <p className='p-[20px] text-center'>
             Stay updated in real-time on your keke driver location to know exactly
             when they will arrive!
           </p>
