849ab58ef41cdb7f23537e70b778099a9b97a4c3
kod-python <kelvinlik0245@gmail.com>
2024-07-23 18:17:19 +0000

    k3k3 commit

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,27 +1,138 @@
 /* eslint-disable react/no-unescaped-entities */
-import React from 'react'
+// import React from 'react'
+
+'use client'
+import React, { ChangeEvent, FormEvent, useState } from "react";
 import Link from 'next/link'
+import { useRouter } from "next/navigation";
 
 const Login = () => {
+
+  const [data, setData] = useState({
+    username: "",
+    password: "",
+  });
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  const router = useRouter();
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setData({
+      ...data,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  // const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+  //   const { name, value } = event.target;
+  //   setData((prev) => ({ ...prev, [name]: value }));
+  // };
+
+  const [passwordVisible, setPasswordVisible] = useState(false);
+
+  const handleSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+    setIsLoading(true);
+
+    try {
+      const response = await fetch("http://localhost:8000/api/login/", {
+        // const response = await fetch("https://rattleviper.pythonanywhere.com/api/login/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error status: ${response.status}`);
+      }
+
+      const responseData = await response.json();
+      console.log(responseData);
+
+      if (responseData.error) {
+        setError(responseData.error);
+      } else {
+     
+        router.push('/WelcomePage');
+      }
+    } catch (error: any) {
+      console.error("Login error:", error);
+      setError('Wrong credential please try login in again!.');
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+
+
   return (
     <div>
 
 <div className='container mx-auto p-[20px] m-[20px]'>
     <h1 className='font-bold text-2xl underline'>Welcome</h1>
     <p>KIndly enter your deatils <br /> lets ride</p>
-    <form action="" className='grid  py-10 gap-[10px]'>
+    <form action=""
+    onSubmit={handleSubmit}
+    className='grid  py-10 gap-[10px]'>
        
         <label  htmlFor="">Username</label>
-        <input className='border rounded-[10px] outline-gray-500 p-[5px]' type="text" />
+        <input className='border rounded-[10px] outline-gray-500 p-[5px]' 
+        type="text"
+        name = "username"
+        id="username"
+        placeholder="username"
+        value={data.username}
+        onChange={handleChange}
+        
+        required
+        />
 
        
 
         <label htmlFor="">Password</label>
-        <input className='border rounded-[10px] outline-gray-500 p-[5px]' type="text" />
+<div className="relative">
 
+
+<input className='border rounded-[10px] outline-gray-500 p-[5px]' 
+        type={passwordVisible ? "text" : "password"}
+        id="password"
+        name = "password"
+        placeholder="password"
+        value={data.password}
+        onChange={handleChange}
+
+        required
+        />
+  <button  type="button" onClick={() => setPasswordVisible(!passwordVisible)} className="absolute right-2 top-2">
+            {passwordVisible ? "Hide password" : "Show password"}
+          </button>
+</div>
+
+     
        
 <div className='pb-[10px]'></div>
-<button type='submit' className='text-white text-1xl py-0.5 border rounded-[15px] bg-red-500'>Login</button>
+<button type='submit' 
+className='text-white text-1xl py-0.5 border rounded-[15px] bg-red-500'>
+
+<div className="flex justify-center items-center">
+            {isLoading ? (
+              <div className="flex items-center gap-3">
+                <p>Loading ....</p>
+              </div>
+            ) : (
+              <p></p>
+            )}
+
+
+Login
+          </div>
+
+</button>
+
+{error && <p className="text-red-500">{error}</p>}
 
 <Link className='text-red-500 underline' href="/WelcomePage">welcome Page</Link>
 
--- src/components/SignUp/SignUp.tsx
+++ src/components/SignUp/SignUp.tsx
@@ -1,45 +1,207 @@
 /* eslint-disable react/no-unescaped-entities */
 
-import React from 'react'
-import Link from 'next/link'
+// import React from 'react'
+'use client'
+import React, { useEffect, useState, ChangeEvent, FormEvent } from "react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const SignUp = () => {
+  const [data, setData] = useState({
+    name: "",
+    username: "",
+    email: "",
+    password: "",
+    confirm_password: "",
+  });
+
+  const router = useRouter();
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const [passwordVisible, setPasswordVisible] = useState(false);
+  const [confirmPasswordVisible, setConfirmPasswordVisible] = useState(false);
+
+
+
+  const handleSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+    setIsLoading(true);
+
+    if (data.password !== data.confirm_password) {
+      setError("Passwords do not match");
+      setIsLoading(false);
+      return;
+    }
+
+    try {
+      const response = await fetch("http://localhost:8000/api/signup/", {
+      // const response = await fetch(
+      //   "https://rattleviper.pythonanywhere.com/api/signup/",
+      //   {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error(`HTTP error status: ${response.status}`);
+      }
+
+      const responseData = await response.json();
+      console.log(responseData);
+
+      if (responseData.error) {
+        console.error(responseData.error);
+        alert(responseData.error);
+      } else {
+        router.push("/LoginPage");
+      }
+
+      setIsLoading(false);
+    } catch (error) {
+      console.error("Error submitting form:", error);
+      alert("A user is already using the same email");
+      setError("A user is already using the same email");
+      setIsLoading(false);
+    }
+  };
+
   return (
     <div>
+      <div className="container mx-auto p-[20px] m-[20px] ">
+        <h1 className="text-center font-bold text-2xl underline">
+          Create An Account
+        </h1>
+        <form
+          action=""
+          onSubmit={handleSubmit}
+          className="grid  py-10 gap-[2px]"
+        >
+          <label htmlFor="">Name</label>
+          <input
+            className="border rounded-[10px] outline-gray-500 p-[5px]"
+            type="text"
+            name="name"
+            placeholder="Name"
+            value={data.name}
+            onChange={handleChange}
+          />
 
-<div className='container mx-auto p-[20px] m-[20px] '>
-    <h1 className='text-center font-bold text-2xl underline'>Create An Account</h1>
-    <form action="" className='grid  py-10 gap-[2px]'>
-        <label htmlFor="">Name</label>
-        <input className='border rounded-[10px] outline-gray-500 p-[5px]' type="text" />
+          <label htmlFor="">Username</label>
+          <input
+            className="border rounded-[10px] outline-gray-500 p-[5px]"
+            type="text"
+            name="username"
+            placeholder="username"
+            value={data.username}
+            onChange={handleChange}
+          />
 
-        <label  htmlFor="">Username</label>
-        <input className='border rounded-[10px] outline-gray-500 p-[5px]' type="text" />
+          <label htmlFor="">Email</label>
+          <input
+            className="border rounded-[10px] outline-gray-500 p-[5px]"
+            type="email"
+            name="email"
+            placeholder="email"
+            value={data.email}
+            onChange={handleChange}
+          />
 
-        <label htmlFor="">Email</label>
-        <input className='border rounded-[10px] outline-gray-500 p-[5px]' type="text"  />
+          <label htmlFor="">Password</label>
 
-        <label htmlFor="">Password</label>
-        <input className='border rounded-[10px] outline-gray-500 p-[5px]' type="text" />
+          <div className="relative">
+          <input
+            className="border rounded-[10px] outline-gray-500 p-[5px]"
+            type={passwordVisible ? "text" : "password"}
+            name="password"
+            placeholder="password"
+            value={data.password}
+            onChange={handleChange}
 
-        <label htmlFor="">Confirm Password</label>
-        <input className='border rounded-[10px] outline-gray-500 p-[5px]' type="text" />
+          />
+          
+          <button  type="button" onClick={() => setPasswordVisible(!passwordVisible)} className="absolute right-2 top-2">
+            {passwordVisible ? "Hide password" : "Show password"}
+          </button>
 
-<div className='pb-[20px]'></div>
-<button type='submit' className='text-white text-1xl py-0.5  border rounded-[15px] bg-red-500'>Create Account</button>
-
-<div className="flex justify-center items-center gap-2">
-<p>Already have an account?</p>
-<Link href="/LoginPage" className='text-red-500 underline'>Sign In</Link>
-</div>
+          </div>
+         
 
 
+          <label htmlFor="">Confirm Password</label>
+
+          <div className="relative">
+
+
+          <input
+            className="border rounded-[10px] outline-gray-500 p-[5px]"
+            type={confirmPasswordVisible ? "text" : "password"}
+            name="confirm_password"
+            placeholder="*************"
+            value={data.confirm_password}
+            onChange={handleChange}
+          />
+
+       <button type="button" onClick={() => setConfirmPasswordVisible(!confirmPasswordVisible)} className="absolute right-2 top-2">
+            {confirmPasswordVisible ? "Hide password" : "Show password"}
+          </button>
+
+
+          </div>
         
-    </form>
+
+
+          <div className="pb-[20px]"></div>
+          <button
+            type="submit"
+            className="text-white text-1xl py-0.5  border rounded-[15px] bg-red-500"
+          >
+
+
+<div className="flex justify-center">
+
+
+{isLoading ? (
+        <div className="flex items-center gap-3">
+          {/* <span className="animate-spin"><FaSpinner  /></span> */}
+          
+          <p>Loading ....</p>
+        </div>
+      ) : (
+        <div>
+         
+          <p></p>
+        </div>
+      )}
+
+      Create Account
 </div>
 
-    </div>
-  )
-}
 
-export default SignUp
+
+            
+          </button>
+          {error && <p className="text-red-500">{error}</p>}
+          <div className="flex justify-center items-center gap-2">
+            <p>Already have an account?</p>
+            <Link href="/LoginPage" className="text-red-500 underline">
+              Sign In
+            </Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
